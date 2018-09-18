b5dd09363b1387d56b1747d10f0b4827029a2cc0
Jacob Queen <jacobsq@protonmail.com>
2018-09-17 22:15:15 -0600

    so much stuff

--- input_handlers.py
+++ input_handlers.py
@@ -12,6 +12,8 @@ def handle_keys(key, game_state):
 		return handle_inventory_keys(key)
 	elif game_state == GameStates.TARGETING:
 		return handle_targeting_keys(key)
+	elif game_state == GameStates.LEVEL_UP:
+		return handle_level_up_menu(key)
 
 	return {}
 
@@ -41,6 +43,8 @@ def handle_player_turn_keys(key):
 		return {'show_inventory': True}
 	elif key_char == 'd':
 		return {'drop_inventory': True}
+	elif key_char == 'q':
+		return {'take_stairs': True}
 
 	if key.vk == libtcod.KEY_ENTER and key.lalt:
 		# Alt+Enter: toggle full screen
@@ -89,6 +93,33 @@ def handle_player_dead_keys(key):
 	elif key.vk == libtcod.KEY_ESCAPE:
 		# Exit the menu
 		return {'exit': True}
+
+	return {}
+
+def handle_main_menu(key):
+	key_char = chr(key.c)
+
+	if key_char == 'a':
+		return {'new_game': True}
+
+	elif key_char == 'b':
+		return {'load_game': True}
+
+	elif key_char == 'c' or key.vk == libtcod.KEY_ESCAPE:
+		return {'exit': True}
+
+	return {}
+
+def handle_level_up_menu(key):
+	if key:
+		key_char = chr(key.c)
+
+		if key_char == 'a':
+			return {'level_up': 'hp'}
+		elif key_char == 'b':
+			return {'level_up': 'str'}
+		elif key_char == 'c':
+			return {'level_up': 'def'}
 
 	return {}
 
